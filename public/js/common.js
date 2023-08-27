27a73732dde1c49831d54b8730c9876e356dfdf6
Pavel <pakhal1414@gmail.com>
2023-08-27 20:39:01 +0200

    done page

---
+++ public/js/common.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const $ = jQuery; 
+
+function eventHandler() {
+
+	JSCCommon.init() 
+
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
+
+// window.onload = function () {
+// 	document.body.classList.add('loaded_hiding');
+// 	window.setTimeout(function () {
+// 		document.body.classList.add('loaded');
+// 		document.body.classList.remove('loaded_hiding');
+// 	}, 500);
+// }
