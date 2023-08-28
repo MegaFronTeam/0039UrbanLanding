0497bd115aa42f7fa7d94b84311e6532ea6382cf
Pavel <pakhal1414@gmail.com>
2023-08-28 10:40:05 +0200

    add scroll img м2

--- public/js/common.js
+++ public/js/common.js
@@ -6,6 +6,48 @@ function eventHandler() {
 
 	JSCCommon.init() 
 
+
+	// onscroll = e => {
+ 
+	// 	const imgs = document.querySelectorAll('.sSituations__img-wrap img'); 
+	// 	const n = Math.floor(pageYOffset / innerHeight); 
+	// 	console.log(n);
+	// 	const m = (pageYOffset % innerHeight) / innerHeight - 4; 
+	// 	imgs.forEach((img, i) => { 
+	// 		i = (n == i) ? 1  : (n + 1 == i) ? m : 0; 
+	// 		// i = Math.max(0, Math.min(1, i)); 
+	// 		img.style.setProperty('--o', i) 
+	// 	})
+	// }
+
+
+
+		var $trigger1 = $('#trigger1');
+		var $trigger2 = $('#trigger2');
+		var $trigger3 = $('#trigger3');
+		let counter = 0;
+
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop() + $(window).height() / 2;
+			var offset1 = $trigger1.offset().top - 200
+			var offset2 = $trigger2.offset().top - 200
+			var offset3 = $trigger3.offset().top - 200
+
+			if (scroll >= offset1) {
+				$('.sSituations img').attr('src', 'img/img-1.jpg')
+				counter = 1;
+			}
+			if (scroll >= offset2) {
+				$('.sSituations img').attr('src', 'img/img-2.jpg')
+				counter = 2;
+			}
+			if (scroll >= offset3) {
+				$('.sSituations img').attr('src', 'img/img-3.jpg')
+				counter = 3;
+			}
+		})
+
+
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
